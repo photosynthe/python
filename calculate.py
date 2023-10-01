22b893abda6372fe4a7c720613dc46dd89916ad3
photosynthe <103929082+photosynthe@users.noreply.github.com>
2023-10-01 14:30:24 +0800

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,16 +1,35 @@
-# 导入用于数学表达式计算的库
-import math
+# 初始化 ans 变量为 None
+ans = None
 
+# 进入无限循环
 while True:
-    # 获取用户输入的数学表达式
-    expression = input("请输入数学表达式：")
+    # 获取用户输入
+    expression = input("请输入表达式（输入'ans'使用上一次的结果，输入'exit'退出程序）: ")
+
+    # 如果用户输入'exit'，则退出循环
+    if expression == 'exit':
+        break
+
+    # 如果用户输入'ans'，则输出上一次的结果
+    if expression == 'ans':
+        if ans is not None:
+            print(f'上一次的结果是: {ans}')
+        else:
+            print('没有上一次的结果')
+        continue
 
     try:
-        # 使用eval()函数计算表达式的结果（请谨慎使用eval，不要接受不受信任的输入）
+        # 使用 eval 函数计算表达式的结果
         result = eval(expression)
 
-        # 打印结果
-        print("计算结果为:", result)
+        # 将结果存储到 ans 变量中
+        ans = result
 
+        # 输出计算结果
+        print(f'计算结果是: {result}')
     except Exception as e:
-        print("发生错误:", e)
+        # 如果计算失败，输出错误信息
+        print(f'计算失败: {e}')
+
+# 退出程序
+print('程序已退出。')
